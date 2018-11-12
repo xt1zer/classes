feb0f213ccd7c4fd21f92dad363ce48dc0e76d99
Daniel <41012295+xt1zer@users.noreply.github.com>
2018-11-12 15:47:05 +0300

    Add files via upload

---
+++ collection.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+template<class T>
+class Collection {
+protected:
+	Collection() {
+
+	}
+	~Collection() {
+
+	}
+
+	virtual void pushBack(T) = 0;
+	virtual const T popBack() = 0;
+	virtual const bool isEmpty() = 0;
+	virtual const size_t size() = 0;
+	virtual void print() = 0;
+	virtual void reverse() = 0;
+};
